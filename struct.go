d38e3c2587816c7a6b8a08d119c94ec3ffa36507
samy291 <144995197+samy291@users.noreply.github.com>
2023-12-15 09:34:10 +0100

    Create struct.go

---
+++ struct.go
@@ -0,0 +1,11 @@
+type HangManData struct {
+	Word              string     // Word composed of '_', ex: H_ll_
+	ToFind            string     // Final word chosen by the program at the beginning. It is the word to find
+	Attempts          int        // Number of attempts left
+	HangmanPositions  [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+	RemainingAttempts int
+	Usedletter        []string
+	dictionaryPath    string
+	letter            string
+	mode			  string
+}
