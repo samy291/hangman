69ac7e73bed220c98beb8370952e998a1e048603
JSAMY <sjoualla@ynov.com>
2023-12-18 19:31:30 +0100

    Hangmanweb edit

--- displayhangman.go
+++ displayhangman.go
@@ -6,15 +6,16 @@ import (
 	"log"
 	"math/rand"
 	"os"
+
 	// "io/ioutil"
 	"encoding/json"
+	"strings"
 	"time"
-	"strings"	
 )
 
 // fonction qui prend un fichier en argument et qui retourne un tableau de mots utilise
 func ListeMot() string {
-    dictionaryPath := os.Args[1]
+	dictionaryPath := os.Args[1]
 	var tabword []string
 	readFile, err := os.Open(dictionaryPath)
 	if err != nil {
@@ -28,56 +29,48 @@ func ListeMot() string {
 		tabword = append(tabword, fileScanner.Text())
 	}
 	readFile.Close()
-    rand.Seed(time.Now().UnixNano()) // Initialize the random number generator
-    max := len(tabword)
-    indexrandomword := rand.Intn(max)
-    randomword := tabword[indexrandomword]
+	rand.Seed(time.Now().UnixNano()) // Initialize the random number generator
+	max := len(tabword)
+	indexrandomword := rand.Intn(max)
+	randomword := tabword[indexrandomword]
 
 	return randomword
 }
+
 // fonction qui prend un mot en argument et qui affiche le mot en underscore avec un nombre de lettre aléatoire afficher
 func Displayword(words string) string {
-    numbreveal := (len(words) /2) - 1
-    tabword := make([]string, len(words))
-    for i := range tabword {
-        tabword[i] = "_"
-    }
-    for i := 0; i < numbreveal; i++ {
-        max := len(words) - 1
-        indexrandomword := rand.Intn(max)
-        randomletter := string(words[indexrandomword])
-        for j := 0; j < len(words); j++ {
-            if string(words[j]) == randomletter {
-                tabword[j] = randomletter
-            }
-        }
-    }
-    return strings.Join(tabword, "")
-}
-
-// fonction qui prend un mot aléatoire dans un tableau de mots entrer en argument
-func Randomword(tabwords []string) string {
-    rand.Seed(time.Now().UnixNano()) // Initialize the random number generator
-    max := len(tabwords)
-    indexrandomword := rand.Intn(max)
-    randomword := tabwords[indexrandomword]
-    return randomword
+	numbreveal := (len(words) / 2) - 1
+	tabword := make([]string, len(words))
+	for i := range tabword {
+		tabword[i] = "_"
+	}
+	for i := 0; i < numbreveal; i++ {
+		max := len(words) - 1
+		indexrandomword := rand.Intn(max)
+		randomletter := string(words[indexrandomword])
+		for j := 0; j < len(words); j++ {
+			if string(words[j]) == randomletter {
+				tabword[j] = randomletter
+			}
+		}
+	}
+	return strings.Join(tabword, "")
 }
 
 // fonction qui prend un mot, une lettre et un tableau de lettres en argument et qui vérifie si la lettre est dans le mot
 func Imputverif(words string, letter string, tabword string) (bool, string) {
-    found := false
-    runes := []rune(tabword)
+	found := false
+	runes := []rune(tabword)
 
-    for j := 0; j < len(words); j++ {
-        if string(words[j]) == letter {
-            runes[j] = rune(letter[0])
-            found = true
-        }
-    }
+	for j := 0; j < len(words); j++ {
+		if string(words[j]) == letter {
+			runes[j] = rune(letter[0])
+			found = true
+		}
+	}
 
-    tabword = string(runes)
-    return found, tabword
+	tabword = string(runes)
+	return found, tabword
 }
 
 func Attempt(attempts int, found bool) int {
@@ -112,70 +105,70 @@ func RemainingAttempts(maxAttempts, usedAttempts int) int {
 }
 
 func PrintHangman(attempts int) {
-    // Lire les frames du fichier hangman séparées par la séquence "========="
-    file, err := os.Open("hangman.txt")
-    if err != nil {
-        fmt.Println("Error: cannot open file")
-        os.Exit(1)
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanLines)
-    var frames []string
-    var frame string     // Frame actuelle
-    for scanner.Scan() { // Lit le fichier ligne par ligne
-        line := scanner.Text()   // Stocke la ligne actuelle
-        if line == "=========" { // Si la ligne est "=========", on a fini une frame
-            frame += line + "\n"
-            frames = append(frames, frame) // On ajoute la frame au tableau de frames
-            frame = ""                     // On réinitialise la frame
-        } else {
-            frame += line + "\n" // On ajoute la ligne à la frame
-        }
-    }
+	// Lire les frames du fichier hangman séparées par la séquence "========="
+	file, err := os.Open("hangman.txt")
+	if err != nil {
+		fmt.Println("Error: cannot open file")
+		os.Exit(1)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var frames []string
+	var frame string     // Frame actuelle
+	for scanner.Scan() { // Lit le fichier ligne par ligne
+		line := scanner.Text()   // Stocke la ligne actuelle
+		if line == "=========" { // Si la ligne est "=========", on a fini une frame
+			frame += line + "\n"
+			frames = append(frames, frame) // On ajoute la frame au tableau de frames
+			frame = ""                     // On réinitialise la frame
+		} else {
+			frame += line + "\n" // On ajoute la ligne à la frame
+		}
+	}
 
-    // Vérifiez que attempts est dans la plage valide pour éviter l'index out of range
-    if attempts >= 0 && attempts < len(frames) {
-        // Afficher la frame correspondant au nombre de vies restantes
-        fmt.Println(frames[len(frames)-attempts-1])
-    }
+	// Vérifiez que attempts est dans la plage valide pour éviter l'index out of range
+	if attempts >= 0 && attempts < len(frames) {
+		// Afficher la frame correspondant au nombre de vies restantes
+		fmt.Println(frames[len(frames)-attempts-1])
+	}
 }
 
 //save function
 func Save(filePath string, data interface{}) error {
-    file, err := os.Create(filePath)
-    if err != nil {
-        return fmt.Errorf("failed to create file: %v", err)
-    }
-    defer file.Close()
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer file.Close()
 
-    jsonData, err := json.Marshal(data)
-    if err != nil {
-        return fmt.Errorf("failed to marshal data: %v", err)
-    }
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to marshal data: %v", err)
+	}
 
-    _, err = file.Write(jsonData)
-    if err != nil {
-        return fmt.Errorf("failed to write data to file: %v", err)
-    }
+	_, err = file.Write(jsonData)
+	if err != nil {
+		return fmt.Errorf("failed to write data to file: %v", err)
+	}
 
-    return nil
+	return nil
 }
 
 //load function
 func Load(filePath string, data interface{}) error {
-    file, err := os.Open(filePath)
-    if err != nil {
-        return fmt.Errorf("failed to open file: %v", err)
-    }
-    defer file.Close()
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
 
-    err = json.NewDecoder(file).Decode(data)
-    if err != nil {
-        return fmt.Errorf("failed to decode data: %v", err)
-    }
+	err = json.NewDecoder(file).Decode(data)
+	if err != nil {
+		return fmt.Errorf("failed to decode data: %v", err)
+	}
 
-    return nil
+	return nil
 }
 
 // fonction qui compare usedletter et letter et qui retourne un bool
@@ -191,26 +184,26 @@ func Compareletter(usedletter []string, letter string) bool {
 // fonction qui prend un mot en argument et qui retourne un tableau de lettre
 func Ascii(word []rune) {
 
-    file, err := os.Open("standard.txt")
-    if err != nil {
-        fmt.Println("Error file:", err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open("standard.txt")
+	if err != nil {
+		fmt.Println("Error file:", err)
+		return
+	}
+	defer file.Close()
 
-    var ascii []string
-    scanner := bufio.NewScanner(file)
+	var ascii []string
+	scanner := bufio.NewScanner(file)
 
-    for scanner.Scan() {
-        ascii = append(ascii, scanner.Text())
+	for scanner.Scan() {
+		ascii = append(ascii, scanner.Text())
 
-    }
+	}
 
-    for i := 0; i < 9; i++ {
-        for j := 0; j < len(word); j++ {
-            fmt.Print(ascii[((int(word[j])-32)*9)+i])
-        }
-        fmt.Print("\n")
-    }
+	for i := 0; i < 9; i++ {
+		for j := 0; j < len(word); j++ {
+			fmt.Print(ascii[((int(word[j])-32)*9)+i])
+		}
+		fmt.Print("\n")
+	}
 
 }
